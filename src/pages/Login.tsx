25ac2c86dd49db9552578b9aeb5f7eb4fd59ce45
Anthony-YV0908 <anthony.yuvega@radenta.com>
2025-01-23 00:34:36 +0800

    this is new login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,7 +6,7 @@ const Login = () => {
       <div className="bg-white p-8 shadow-md w-full max-w-md">
         <form>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2">Email</label>
+            <label className="block text-gray-700 text-sm font-bold mb-2">Emails</label>
             <input type="email" id="email" name="email" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Enter your email" required/>
           </div>
 
